f161d06595383e39aedac8a875b70c0907fa7c16
JorgeTrivilin <jorge.trivilin@gmail.com>
2024-10-21 16:31:45 -0300

    First commit

---
+++ src/main/java/com/cofrinho/Dolar.java
@@ -0,0 +1,19 @@
+package com.cofrinho;
+
+public class Dolar extends Moeda {
+
+    public Dolar(double valor) {
+        super(valor);
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Dólar - Valor: $" + valor);
+    }
+
+    @Override
+    public double converter() {
+        return valor * 5.00;
+    }
+}
+
